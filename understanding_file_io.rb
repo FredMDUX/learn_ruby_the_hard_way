abb017f4c2df7dcf8bbf02e5d3460fadf33ee6cf
Frederick Mlungisi Dandure <frederickdandure@gmail.com>
2019-02-07 07:37:18 +0200

    Block convention on files

--- understanding_file_io.rb
+++ understanding_file_io.rb
@@ -2,3 +2,8 @@ fname = "understanding_file_io.txt"
 somefile = File.open(fname, "w")
 somefile.puts "Hello file!"
 somefile.close
+
+File.open("understanding_file_io.txt", "a") {
+  |somefile|
+  somefile.puts "It's me again!"
+}
