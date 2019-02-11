acf646f8dbcec4e76ddd09000d0895da6e92c9b9
Frederick Mlungisi Dandure <frederickdandure@gmail.com>
2019-02-11 09:38:11 +0200

    more practice from prev exercises --

---
+++ ex24.rb
@@ -0,0 +1,36 @@
+puts "Let's practice everything."
+puts 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
+
+# the <<END is a "heredoc"
+poem = <<END
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere ther is none.
+END
+
+puts "---------------------"
+puts poem
+puts "---------------------"
+
+five = 10 - 2 + 3 - 6
+puts "this should be five: #{five}"
+
+def secret_formula(started)
+  jelly_beans = started * 500
+  jars = jelly_beans / 1000
+  crates = jars / 100
+  return jelly_beans, jars, crates
+end
+
+start_point = 10000
+beans, jars, crates = secret_formula(start_point)
+
+puts "with a starting point of: #{start_point}"
+puts "We'd have #{beans} beans, #{jars} jars, and #{crates} crates."
+
+start_point /= 10
+puts "We can do that this way:"
+puts "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
