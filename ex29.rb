e2f360954171a694d34b9deb30d935d026d78841
Frederick Mlungisi Dandure <frederickdandure@gmail.com>
2019-02-12 15:01:28 +0200

    if statements --

---
+++ ex29.rb
@@ -0,0 +1,34 @@
+# because there's no exercise 27 and 28
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats
+  puts "Too many cats! The world is doomed!"
+end
+
+if people > cats
+  puts "Not many cats! The world is saved!"
+end
+
+if people < dogs
+  puts "The world is drooled on!"
+end
+
+if people > dogs
+  puts "The world is dry!"
+end
+
+dogs += 5
+
+if people >= dogs
+  puts "people are more than or equal in number to dogs."
+end
+
+if people <= dogs
+  puts "people are less than or equal to dogs."
+end
+
+if people == dogs
+  puts "people are dogs."
+end 
