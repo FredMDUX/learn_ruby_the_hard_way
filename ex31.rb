bec12e2894888f85a9e6b6b73030435abd69d2dc
Frederick Mlungisi Dandure <frederickdandure@gmail.com>
2019-02-12 17:38:23 +0200

    hey - i just did complex control flow - yayyy!

---
+++ ex31.rb
@@ -0,0 +1,40 @@
+# Making Decisions
+
+def prompt
+  print "> "
+end
+
+puts "You enter a dark room with two doors. Do you go through door #1 or door #2?"
+prompt; door = $stdin.gets.chomp
+
+if door == "1" or door == "#1" or door == "one"
+  puts "There's a giant bear here eating a cheese cake. What do you do?"
+  puts "1. Take the cake."
+  puts "2. Scream at the bear."
+
+  prompt; bear = gets.chomp
+
+  if bear == "1" or bear == "1." or bear == "one"
+    puts "The bear eats your face off. Good job!"
+  elsif bear == "2"
+    puts "The bear eats your legs off. Good job!"
+  else
+    puts "Well, doing #{bear} is probably better. Bear runs away."
+  end
+
+elsif door == "2" or door == "#2" or door == "two"
+  puts "You stare into the endless abyss at at Cthuhlu's retina."
+  puts "1. Blueberries."
+  puts "2. Yellow jacket clothespins."
+  puts "3. Understanding revolvers yelling melodies."
+
+  prompt; insanity = gets.chomp
+
+  if insanity == "1" or insanity == "2"
+    puts 'Your body survives powered by a mind of jello. Good job!'
+  else
+    puts "The insanity rots your eyes into a pool of muck. Good job!"
+  end
+else
+  puts "This program is crazy stupid lol!!!"
+end
